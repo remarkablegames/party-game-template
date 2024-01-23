741e75eac66b2160e154351724573ea77ef5f480
Mark <mark@remarkablemark.org>
2024-01-22 21:04:07 -0500

    feat(pages): add heading and button to Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,17 @@
-import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 export default function Home() {
   return (
-    <Typography>
-      Welcome to{' '}
-      <Link
-        href="https://github.com/remarkablegames/party-game-template"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+    <Box sx={{ textAlign: 'center' }}>
+      <Typography component="h1" variant="h2" gutterBottom>
         Party Game Template
-      </Link>
-      !
-    </Typography>
+      </Typography>
+
+      <Button variant="contained" size="large">
+        Create room
+      </Button>
+    </Box>
   );
 }
