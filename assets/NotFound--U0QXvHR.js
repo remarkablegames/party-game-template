a7efe885e6a17411def8fd8f7e1019162e7ef45e
remarkablemark <remarkablemark@users.noreply.github.com>
2024-01-24 04:31:17 +0000

    Deploying to gh-pages from @ remarkablegames/party-game-template@bf694d885d1d31b64354b11ebcffa6aa72711f5e 🚀

---
+++ assets/NotFound--U0QXvHR.js
@@ -0,0 +1,1 @@
+import{j as o,T as r,L as t,k as n}from"./index-ns_PPSyE.js";import{u as e}from"./useSetDocumentTitle--zau9zOp.js";function i(){return e("Not Found"),o.jsxs(o.Fragment,{children:[o.jsx(r,{component:"h1",paragraph:!0,variant:"h4",children:"Not Found"}),o.jsxs(r,{paragraph:!0,children:["Go"," ",o.jsx(t,{component:n,to:"/",children:"home"}),"."]})]})}export{i as default};
