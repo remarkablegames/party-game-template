91a659b12f7493d0d6f9735c729ae802c9e49d62
remarkablemark <remarkablemark@users.noreply.github.com>
2024-01-24 04:37:03 +0000

    Deploying to gh-pages from @ remarkablegames/party-game-template@7dfd5049a2529042b19777c247f6d5fb6be6a0f1 🚀

--- assets/useSetDocumentTitle--zau9zOp.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-ns_PPSyE.js";function r(t){e.useEffect(()=>{document.title=t})}export{r as u};
---
+++ assets/useSetDocumentTitle-kgv3RLWV.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-iqlCKQXH.js";function r(t){e.useEffect(()=>{document.title=t})}export{r as u};
