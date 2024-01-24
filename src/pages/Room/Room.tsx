f849242fa04162f6fa9d96b3cbd496064bc60dc9
Mark <mark@remarkablemark.org>
2024-01-23 21:46:22 -0500

    chore(pages): eliminate dead code in Room

--- src/pages/Room/Room.tsx
+++ src/pages/Room/Room.tsx
@@ -1,13 +1,10 @@
 import { DriftDBProvider } from 'driftdb-react';
-import { useParams } from 'react-router-dom';
 import RoomDetails from 'src/components/RoomDetails';
 import { DRIFTDB_URL } from 'src/config';
 
 export default function Room() {
-  const { room } = useParams<{ room: string }>();
-
   return (
-    <DriftDBProvider api={DRIFTDB_URL} room={room}>
+    <DriftDBProvider api={DRIFTDB_URL}>
       <RoomDetails />
     </DriftDBProvider>
   );
