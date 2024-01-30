c28262d95307466c095c5fa713efda9ce9c8cdd7
Mark <mark@remarkablemark.org>
2024-01-29 20:48:29 -0500

    feat: add google analytics

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,3 +3,4 @@ export const APP_VERSION = import.meta.env.VITE_APP_VERSION;
 export const BASE_URL = import.meta.env.BASE_URL;
 export const DEV = import.meta.env.DEV;
 export const DRIFTDB_URL = import.meta.env.VITE_DRIFTDB_URL;
+export const GOOGLE_ANALYTICS_ID = import.meta.env.VITE_GOOGLE_ANALYTICS_ID;
--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -6,6 +6,7 @@ interface ImportMetaEnv {
   readonly VITE_APP_NAME: string;
   readonly VITE_APP_VERSION: string;
   readonly VITE_DRIFTDB_URL: string;
+  readonly VITE_VITE_GOOGLE_ANALYTICS_ID: string;
 }
 
 interface ImportMeta {
